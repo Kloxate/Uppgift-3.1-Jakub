06f5d21b1772fcbed87f3e60d58acea86ba07dab
Admin <Admin@TEB123012>
2023-10-20 15:21:10 +0200

    Add project files.

---
+++ Uppgift 3.1 Jakub/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppgift_3_1Jakub
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            string ålder = Console.ReadLine();
+            int åldernmr = int.Parse(ålder);
+
+            if (åldernmr > 19)
+            {
+                Console.WriteLine("Du är för gammal för att delta i tävlingen.");
+            }
+            if (åldernmr < 16)
+            {
+                Console.WriteLine("Du är för ung för att delta i tävlingen.");
+            }
+            else if (åldernmr >= 16 && åldernmr <= 19)
+            {
+                Console.WriteLine("Du kan delta i tävlingen!");
+            }
+        }
+    }
+
+}
